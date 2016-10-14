6f56fbbbec771654e51ad07272ea6e57bbb4ec9a
Iavor S. Diatchki <diatchki@galois.com>
2016-10-14 10:58:17 -0700

    UI improvements: make to context menues look nicer.

--- ui/new/popUpMenu.js
+++ ui/new/popUpMenu.js
@@ -5,7 +5,7 @@ function icon(cls,i) {
 function roundButton(cls,i,help,act) {
   var me = $('<a/>').addClass(cls)
            .addClass('btn-floating waves-effect waves-light tooltipped')
-           .css('margin-right','10px')
+           .css('margin-left','10px')
            .attr('data-tooltip',help)
            .append(icon('',i))
            .click(act)
@@ -19,34 +19,47 @@ function makeMenu(thing,btns) {
   var openWidth = btns.length * 50
   var container = $('<div/>')
                   .css('width', openWidth + 'px')
-                  .css('height','40px')
+                  .css('height','50px')
                   .css('position','absolute')
-                  .css('left','0px')
-                  .css('top','0px')
+                  .css('z-index','10')
                   .append(btns)
                   .hide()
   thing.css('cursor','pointer')
+  $('body').append(container)
   var open = false
 
-  var outer = $('<div/>').css('display','inline-block')
-                         .css('position','relative')
+  var allBtn = container.find('.btn-floating')
+                        .velocity({ scaleX: 0.2, scaleY: 0.2, opacity: 0 }, 0)
+
+  var busy = false
 
   function showMenu() {
-      container.css('left',(thing.width() + 10) + 'px')
-      outer.addClass('pop_up_open')
-      container.show()
-      open = true
+      if (busy) return
+      busy = true
+      var thingPagePos = thing.offset()
+      var hiddenAbove  = $('body').scrollTop()
+      var left = thingPagePos.left + 10
+      var top  = thingPagePos.top + thing.height() + 10
+
+      container.css('left', left)
+               .css('top', top)
+               .show()
+
+      allBtn.velocity({ scaleX: 1, scaleY: 1, opacity: 1 }, 80, 'swing'
+                      , function () { open = true; busy = false }
+                      )
   }
 
   function hideMenu() {
-    container.hide()
-    outer.removeClass('pop_up_open')
-    open = false
+    if (busy) return
+    busy = true
+    allBtn.velocity( { scaleX: 0.2, scaleY: 0.2, opacity: 0 }, 80, 'linear'
+                   , function() { open = false; busy = false } )
   }
 
   thing.click(function() { if (open) hideMenu(); else showMenu() })
   jQuery.each(btns,function(ix,b) { b.click(hideMenu) })
 
-  return outer.append([thing,container])
+  return thing
 }
 
