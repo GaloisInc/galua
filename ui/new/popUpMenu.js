cc1b95393129d3735dfe39e6d3d60d3f8a2ad2db
Iavor S. Diatchki <diatchki@galois.com>
2016-10-17 14:10:53 -0700

    UI improvements: context menus
    	* only one context menu can be open at a time
       	* add a timer to close the menu automatically, after 2s

--- ui/new/popUpMenu.js
+++ ui/new/popUpMenu.js
@@ -25,6 +25,7 @@ function makeMenu(thing,btns) {
                   .append(btns)
                   .hide()
   thing.css('cursor','pointer')
+       .addClass('galua-menu-thing')
   $('body').append(container)
   var open = false
 
@@ -33,9 +34,29 @@ function makeMenu(thing,btns) {
 
   var busy = false
 
+  var timer = null
+
+  function startTimer() {
+    stopTimer()
+    timer = window.setTimeout(hideMenu, 2 * 1000)
+  }
+
+  function stopTimer() {
+    if (timer !== null) window.clearTimeout(timer)
+    timer = null
+  }
+
+
+  jQuery.each(btns,function(ix,b) {
+    b.click(hideMenu).hover(stopTimer,startTimer)
+  })
+
   function showMenu() {
       if (busy) return
       busy = true
+      jQuery.each($('.galua-menu-thing'),function(ix,th) {
+        if (th !== thing) $(th).data('galua-popup-menu-hide')()
+      })
       var thingPagePos = thing.offset()
       var hiddenAbove  = $('body').scrollTop()
       var left = thingPagePos.left + 10
@@ -46,20 +67,20 @@ function makeMenu(thing,btns) {
                .show()
 
       allBtn.velocity({ scaleX: 1, scaleY: 1, opacity: 1 }, 80, 'swing'
-                      , function () { open = true; busy = false }
+                      , function () { open = true; busy = false; startTimer() }
                       )
   }
 
   function hideMenu() {
     if (busy) return
     busy = true
+    stopTimer()
     allBtn.velocity( { scaleX: 0.2, scaleY: 0.2, opacity: 0 }, 80, 'linear'
                    , function() { open = false; busy = false } )
   }
 
   thing.click(function() { if (open) hideMenu(); else showMenu() })
-  jQuery.each(btns,function(ix,b) { b.click(hideMenu) })
 
-  return thing
+  return thing.data('galua-popup-menu-hide', hideMenu)
 }
 
