ef491c61ace73a8fa4168147af63a36274746c3b
Eric Mertens <emertens@gmail.com>
2016-10-20 14:30:28 -0700

    Add script for constructing Lua.framework

--- galua-c/mk_macos_shared.sh
+++ galua-c/mk_macos_shared.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cc -shared -install_name '@rpath/Lua.framework/Versions/A/Lua' -o Lua -Linplace/lib -lgalua-dbg -liconv -lz
+rm -rf Lua.framework/Versions/A/Headers
+mkdir -p Lua.framework/Versions/A/Headers
+cp inplace/include/galua/*.h Lua.framework/Versions/A/Headers/
+cc \
+        -shared \
+        -install_name '@rpath/Lua.framework/Versions/A/Lua' \
+        -o Lua.framework/Versions/A/Lua \
+        -Linplace/lib \
+        -lgalua-dbg -liconv -lz
