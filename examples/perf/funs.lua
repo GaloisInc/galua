c3dec16263d5ea864a5c0af865719f0ab2e26bc8
Iavor S. Diatchki <diatchki@galois.com>
2016-12-13 09:55:23 -0800

    Some more tests

---
+++ examples/perf/funs.lua
@@ -0,0 +1,21 @@
+local function f(i,sum)
+  return i-1, sum + 1
+end
+
+
+local sum = 0
+local j = 1000
+
+while (j > 0) do
+
+  local i = 10000
+
+  while (i > 0) do
+    i,sum = f(i,sum)
+  end
+
+  j = j - 1
+end
+
+print (sum)
+
---
+++ examples/perf/loops1.lua
@@ -0,0 +1,13 @@
+
+local sum = 0
+local maxJ = 10000
+
+for j = 1,maxJ do
+
+  for i = 1,10000 do
+    sum = sum + 1
+  end
+end
+
+print (sum)
+
